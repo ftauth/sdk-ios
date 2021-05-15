168b74b41d44434360b6375fb867d0bd0109c611
Dillon Nys <dnys1@asu.edu>
2021-05-15 14:28:14 -0700

    Add tests

--- Package.swift
+++ Package.swift
@@ -12,7 +12,7 @@ let package = Package(
         // Products define the executables and libraries a package produces, and make them visible to other packages.
         .library(
             name: "FTAuth",
-            targets: ["FTAuth", "FTAuthInternal"]),
+            targets: ["FTAuth", "FTAuthTests"]),
     ],
     dependencies: [
         // Dependencies declare other packages that this package depends on.
